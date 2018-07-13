08c52f4c05772aced2d743cd3bd724754d5deb5c
Patricio Gonzalez Leal <patonxs@gmail.com>
2018-07-12 23:54:20 -0400

    comment

--- db.py
+++ db.py
@@ -1,12 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 
 #app.config.from_pyfile('config.db.cfg', silent=True)
-user = 'pato'
-password = 'inxs0175'
-host = '144.217.240.227'
-port = 5432
-database = 'postgres'
-uri = "postgresql://pato:inxs0175@144.217.240.227/postgres?client_encoding=utf8"
+user = 'user'
+password = 'pass'
+host = 'host'
+port = 111111
+database = 'db'
+uri = "dribver://user:pass@host"
 
 db = SQLAlchemy()
 
